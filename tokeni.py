b0b8fa0ee70acc27e11ec3cd2cea343911860ff3
treezyyy <122749977+treezyyy@users.noreply.github.com>
2023-12-23 00:44:40 +0300

    Update tokeni.py

--- tokeni.py
+++ tokeni.py
@@ -1,5 +1,5 @@
-Token = '6329301918:AAHMDVxQflx38taLaWB40_LmtqhZGjj-EeU'
+Token = 'ай--ай'
 
-Api_key = 'test_RTySw4rKy9p3kbFsNGQddFhA2hRk4Xx4Ept2mR_WEG8'
+Api_key = 'ай-ай'
 
-Api_id = '296528'
+Api_id = 'ай-ай'
